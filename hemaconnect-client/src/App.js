ce397460f2055e12307d770c3c8d1d13ceb84090
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-10 01:26:31 +0300

    Client <> Adding Routing

--- hemaconnect-client/src/App.js
+++ hemaconnect-client/src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
 import Navbar from "./components/ui/Navbar";
+import React, {useState, useEffect} from "react";
+import Login from "./pages/Login";
+import Bank from "./pages/Bank";
+import Bookings from "./pages/Bookings";
+import {Routes, Route} from "react-router-dom";
+
 function App() {
-  return (
-<Navbar/>
-  );
+
+    const [authenticated, setauthenticated] = useState(null);
+    useEffect(() => {
+        const loggedInUser = localStorage.getItem("authenticated");
+        if (loggedInUser) {
+            setauthenticated(loggedInUser);
+        }
+    }, []);
+    if (!authenticated) {
+        return (
+            <Routes>
+                <Route path="/" element={<Login/>}/>
+            </Routes>
+        );
+    }
+    return (
+        <div>
+            <Navbar/>
+            <Routes>
+                <Route path="/bank" element={<Bank/>}/>
+                <Route path="/bookings" element={<Bookings/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ hemaconnect-client/src/pages/Bank/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles.css';
+
+const Bank = () => {
+    return(
+        <div>
+            Bank
+        </div>
+    )
+}
+
+export default Bank;
