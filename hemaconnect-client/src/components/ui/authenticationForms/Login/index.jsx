1868067b2d8878de1a4442ef3e6a9c7f6896eabf
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-09 17:27:49 +0300

    Client <> Adding Login Validations

--- hemaconnect-client/src/components/ui/authenticationForms/Login/index.jsx
+++ hemaconnect-client/src/components/ui/authenticationForms/Login/index.jsx
@@ -1,35 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./styles.css";
 import Input from "../../../base/Input";
 import Button from "../../../base/Button";
-import Hema from '../../../../assets/SVGs/Hema.svg'
-import Connect from '../../../../assets/SVGs/Connect.svg'
-
+import Hema from '../../../../assets/SVGs/Hema.svg';
+import Connect from '../../../../assets/SVGs/Connect.svg';
+import axios from "axios";
 
 const LoginForm = () => {
+    const [authenticated, setauthenticated] = useState(
+        localStorage.getItem("authenticated") || false
+    );
+    const [credentials, setCredentials] = useState({
+        email: "",
+        password: "",
+    });
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+        invalidCredentials: "",
+    });
+
+    const handleDataChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value });
+        setErrors({ ...errors, [e.target.name]: "" });
+        setErrors({
+            email: "",
+            password: "",
+            invalidCredentials: "",
+        });
+        if (e.target.name === "email") {
+            setErrors({ ...errors, email: "" });
+        }
+        if (e.target.name === "password") {
+            setErrors({ ...errors, password: "" });
+        }
+    };
+
+    const handleLogin = async () => {
+        let formIsValid = true;
+        const newErrors = {};
+
+        if (!credentials.email.trim()) {
+            newErrors.email = "Please enter email";
+            formIsValid = false;
+        }
+
+        if (!credentials.password.trim()) {
+            newErrors.password = "Please enter password";
+            formIsValid = false;
+        }
+
+        if (!formIsValid) {
+            setErrors(newErrors);
+            return;
+        }
+
+        try {
+            const response = await axios.post(
+                "http://127.0.0.1:8000/api/login",
+                credentials
+            );
+            if (response.status === 200) {
+                const responseData = response?.data;
+                if (responseData) {
+                    setauthenticated(true);
+                    localStorage.setItem("authenticated", true);
+                    localStorage.setItem("token", responseData.authorization.token);
+                    window.location.reload(false);
+                }
+            }
+        } catch (error) {
+            console.error("Login error:", error);
+            setErrors({ ...errors, invalidCredentials: "Invalid credentials" });
+        }
+    };
+
     return (
         <div className="login-form">
             <div className="login-container">
                 <div>
-                    <img src={Hema} alt="Hema Logo"/>
-                    <img src={Connect} alt="Hema Logo"/>
+                    <img src={Hema} alt="Hema Logo" />
+                    <img src={Connect} alt="Hema Logo" />
                 </div>
                 <div className="cred-btn-wrapper">
                     <div>
-                    <Input
-                        placeholder="Email"
-                    />
-                    <Input
-                        placeholder="Password"
-                        type="password"
-                    />
+                        <Input
+                            placeholder="Email"
+                            value={credentials.email}
+                            onChange={handleDataChange}
+                            name="email"
+                            type="text"
+                            className={errors.email ? "input-error" : ""}
+                        />
+                        {errors.email && (
+                            <div className="error-message">{errors.email}</div>
+                        )}
+                        <Input
+                            placeholder="Password"
+                            value={credentials.password}
+                            onChange={handleDataChange}
+                            type="password"
+                            name="password"
+                            className={errors.password ? "input-error" : ""}
+                        />
+                        {errors.password && (
+                            <div className="error-message">{errors.password}</div>
+                        )}
+                        {errors.invalidCredentials && (
+                            <div className="error-message">{errors.invalidCredentials}</div>
+                        )}
                     </div>
                     <Button
                         label="Login"
+                        onClick={handleLogin}
                     />
                 </div>
             </div>
-
         </div>
     );
 };
