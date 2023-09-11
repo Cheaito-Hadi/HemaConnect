d928ef4cff4b60b0a85b1ff340c4c73e0cd58d01
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-12 01:45:02 +0300

    Server <> Updating migration

---
+++ HemaConnect-Server/database/migrations/2023_09_11_223629_update_booking_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::dropIfExists('bookings');
+
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('time');
+            $table->boolean('hepatitis');
+            $table->boolean('anemia');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('hospital_id');
+            $table->foreign('hospital_id')->references('id')->on('hospitals')->onDelete('cascade');
+            $table->unsignedBigInteger('request_id');
+            $table->foreign('request_id')->references('id')->on('blood_requests')->onDelete('cascade');
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::dropIfExists('bookings');
+
+        Schema::enableForeignKeyConstraints();
+    }
+};
