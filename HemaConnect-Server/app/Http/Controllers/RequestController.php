e4376f1d34849c1dbdc1866609d0fbc4e66da206
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-11 16:51:24 +0300

    Server <> Updating createRequest with Accumulated Blood type

--- HemaConnect-Server/app/Http/Controllers/RequestController.php
+++ HemaConnect-Server/app/Http/Controllers/RequestController.php
@@ -14,6 +14,18 @@ class RequestController extends Controller
         if (is_null($user)) {
             return response()->json(["message" => 'failed']);
         }
+
+        $existingRequest = BloodRequest::where('bloodtypes_id', $request->bloodtype)
+            ->where('hospital_id', $user->employees[0]->hospital->id)
+            ->first();
+
+        if ($existingRequest) {
+            $existingRequest->needed_amount += $request->needed_amount;
+            $existingRequest->save();
+
+            return response()->json(["Blood Request" => $existingRequest]);
+        }
+
         $new_request = new BloodRequest;
         $new_request->bloodtypes_id = $request->bloodtype;
         $new_request->needed_amount = $request->needed_amount;
