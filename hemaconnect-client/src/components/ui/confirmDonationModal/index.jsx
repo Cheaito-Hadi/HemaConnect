4104a6014da8ff25d613a3c36e75db9876543f3d
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-13 17:50:46 +0300

    Client <> Styling donation modal

--- hemaconnect-client/src/components/ui/confirmDonationModal/index.jsx
+++ hemaconnect-client/src/components/ui/confirmDonationModal/index.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './styles.css';
 import XMark from '../../../assets/SVGs/XMark.svg'
 
-const DonationModal = ({ onClose, onSubmit, selectedBooking }) => {
+const DonationModal = ({onClose, onSubmit, selectedBooking}) => {
     const [donatedAmount, setDonatedAmount] = useState('');
 
     const handleDonatedAmountChange = (e) => {
@@ -16,28 +16,32 @@ const DonationModal = ({ onClose, onSubmit, selectedBooking }) => {
     };
 
     return (
-        <div className="modal-content">
-            <div className="modal-request-title">
+        <div className="modal-content-donation">
+            <div className="modal-donation-title">
                 <h2>Confirm Donation</h2>
                 <img src={XMark} alt="Close" onClick={onClose}/>
             </div>
             <div className="modal-request-info">
-                <p>Name: {selectedBooking.user_name}</p>
-                <p>Booking: {new Date(selectedBooking.time).toLocaleString()}</p>
-                <p>Blood Type: {selectedBooking.user_blood_type}</p>
-            </div>
-            <div className="amount-requested">
-                <label htmlFor="donatedAmount">Donated Amount (Kg):</label>
-                <input
-                    type="number"
-                    id="donatedAmount"
-                    placeholder="Enter donated amount"
-                    value={donatedAmount}
-                    onChange={handleDonatedAmountChange}
-                    step="0.01"
-                    min="0"
-                    required
-                />
+                <p className="title-info-wrapper"><span className="donation-title"> Name: </span> <span
+                    className="donation-info"> {selectedBooking.user_name}</span></p>
+                <p className="title-info-wrapper"><span className="donation-title">Booking: </span><span
+                    className="donation-info">{new Date(selectedBooking.time).toLocaleString()}</span></p>
+                <p className="title-info-wrapper"><span className="donation-title">Blood Type: </span><span
+                    className="donation-info">{selectedBooking.user_blood_type}</span></p>
+                <div className="amount-requested">
+                    <label className="donated-amount-title" htmlFor="donatedAmount">Donated Amount (Kg):</label>
+                    <input
+                        className="donated-amount"
+                        type="number"
+                        id="donatedAmount"
+                        placeholder="Enter donated amount"
+                        value={donatedAmount}
+                        onChange={handleDonatedAmountChange}
+                        step="0.01"
+                        min="0"
+                        required
+                    />
+                </div>
             </div>
             <div className="modal-button">
                 <button className="confirm-button" onClick={handleSubmit}>Confirm</button>
