342df419a39c962d51c7ee56783d7096af98c291
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-15 10:28:43 +0300

    Client <> Editing table

--- hemaconnect-client/src/components/ui/BookingTable/index.jsx
+++ hemaconnect-client/src/components/ui/BookingTable/index.jsx
@@ -11,74 +11,76 @@ const TABLE_HEAD = ["Name", "Email", "Booking Time", "Blood Type", "Anemia", "He
 export function BookingTable({ bookingData, onDelete, onConfirmDonation }) {
     return (
         <Card className="h-full w-full">
-            <table className="w-full min-w-max table-auto text-left">
-                <thead>
-                <tr>
-                    {TABLE_HEAD.map((head) => (
-                        <th key={head} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                            <Typography
-                                variant="small"
-                                color="black"
-                                className="font-bold leading-none opacity-70 align-middle"
-                            >
-                                {head}
-                            </Typography>
-                        </th>
-                    ))}
-                </tr>
-                </thead>
-                <tbody>
-                {bookingData.map((booking, index) => (
-                    <tr
-                        key={booking.id}
-                        className={`${
-                            index % 2 === 0 ? "bg-white" : "bg-gray-100"
-                        } ${booking.donated ? "confirmed-donation" : ""}`}
-                    >
-                        <td className="p-4 align-middle" style={{ height: '50px' }}>
-                            <Typography variant="small" color="blue-gray" className="font-normal">
-                                {booking.user_name}
-                            </Typography>
-                        </td>
-                        <td className="p-4 align-middle" style={{ height: '50px' }}>
-                            <Typography variant="small" color="blue-gray" className="font-normal">
-                                {booking.user_email}
-                            </Typography>
-                        </td>
-                        <td className="p-4 align-middle" style={{ height: '50px' }}>
-                            <Typography variant="small" color="blue-gray" className="font-normal">
-                                {new Date(booking.time).toLocaleString()}
-                            </Typography>
-                        </td>
-                        <td className="p-4 align-middle" style={{ height: '50px' }}>
-                            <Typography variant="small" color="blue-gray" className="font-normal">
-                                {booking.user_blood_type}
-                            </Typography>
-                        </td>
-                        <td className="p-4 align-middle" style={{ height: '50px' }}>
-                            <Typography variant="small" color="blue-gray" className="font-normal">
-                                {booking.anemia === 1 ? <CheckIcon color="primary" /> : <CloseIcon color="error" />}
-                            </Typography>
-                        </td>
-                        <td className="p-4 align-middle" style={{ height: '50px' }}>
-                            <Typography variant="small" color="blue-gray" className="font-normal">
-                                {booking.hepatitis === 1 ? <CheckIcon color="primary" /> : <CloseIcon color="error" />}
-                            </Typography>
-                        </td>
-                        <td className="p-4 align-middle" style={{ height: '50px', cursor: 'pointer' }}>
-                            <Typography variant="small" color="blue" className="font-normal" onClick={() => onConfirmDonation(booking)}>
-                                <ThumbUpIcon />
-                            </Typography>
-                        </td>
-                        <td className="p-4 align-middle" style={{ height: '50px', cursor: 'pointer' }}>
-                            <Typography variant="small" className="font-normal" onClick={() => onDelete(booking.id)}>
-                                <DeleteIcon color="error" />
-                            </Typography>
-                        </td>
+            <div className="table-container" style={{ maxHeight: '600px', overflowY: 'auto' }}>
+                <table className="w-full min-w-max table-auto text-left">
+                    <thead>
+                    <tr>
+                        {TABLE_HEAD.map((head) => (
+                            <th key={head} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                                <Typography
+                                    variant="small"
+                                    color="black"
+                                    className="font-bold leading-none opacity-70 align-middle"
+                                >
+                                    {head}
+                                </Typography>
+                            </th>
+                        ))}
                     </tr>
-                ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    {bookingData.map((booking, index) => (
+                        <tr
+                            key={booking.id}
+                            className={`${
+                                index % 2 === 0 ? "bg-white" : "bg-gray-100"
+                            } ${booking.donated ? "confirmed-donation" : ""}`}
+                        >
+                            <td className="p-4 align-middle" style={{ height: '50px' }}>
+                                <Typography variant="small" color="blue-gray" className="font-normal">
+                                    {booking.user_name}
+                                </Typography>
+                            </td>
+                            <td className="p-4 align-middle" style={{ height: '50px' }}>
+                                <Typography variant="small" color="blue-gray" className="font-normal">
+                                    {booking.user_email}
+                                </Typography>
+                            </td>
+                            <td className="p-4 align-middle" style={{ height: '50px' }}>
+                                <Typography variant="small" color="blue-gray" className="font-normal">
+                                    {new Date(booking.time).toLocaleString()}
+                                </Typography>
+                            </td>
+                            <td className="p-4 align-middle" style={{ height: '50px' }}>
+                                <Typography variant="small" color="blue-gray" className="font-normal">
+                                    {booking.user_blood_type}
+                                </Typography>
+                            </td>
+                            <td className="p-4 align-middle" style={{ height: '50px' }}>
+                                <Typography variant="small" color="blue-gray" className="font-normal">
+                                    {booking.anemia === 1 ? <CheckIcon color="primary" /> : <CloseIcon color="error" />}
+                                </Typography>
+                            </td>
+                            <td className="p-4 align-middle" style={{ height: '50px' }}>
+                                <Typography variant="small" color="blue-gray" className="font-normal">
+                                    {booking.hepatitis === 1 ? <CheckIcon color="primary" /> : <CloseIcon color="error" />}
+                                </Typography>
+                            </td>
+                            <td className="p-4 align-middle" style={{ height: '50px', cursor: 'pointer' }}>
+                                <Typography variant="small" color="blue" className="font-normal" onClick={() => onConfirmDonation(booking)}>
+                                    <ThumbUpIcon />
+                                </Typography>
+                            </td>
+                            <td className="p-4 align-middle" style={{ height: '50px', cursor: 'pointer' }}>
+                                <Typography variant="small" className="font-normal" onClick={() => onDelete(booking.id)}>
+                                    <DeleteIcon color="error" />
+                                </Typography>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
         </Card>
     );
 }
