f628ef923439f081399f8f6cb71190cb5973ecb1
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-15 15:08:05 +0300

    Mobile <> Creating Button component

--- HemaConnect-Mobile/App.js
+++ HemaConnect-Mobile/App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, View} from 'react-native';
+import Button from "./components/base/customedButton";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, I'm Hadi</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.loginScreen}>
+                <Button buttonTitle="Log In"/>
+            </View>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#F7F0F3',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    loginScreen: {
+        width: '80%',
+        alignItems: 'center',
+    },
 });
---
+++ HemaConnect-Mobile/components/base/customedButton.js
@@ -0,0 +1,38 @@
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+
+const Button = (props) => {
+    return (
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                    handlePress();
+                }}
+            >
+                <Text style={styles.buttonText}>{props.buttonTitle}</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        width: '100%',
+        alignItems: 'center',
+    },
+    button: {
+        borderRadius: 10,
+        backgroundColor: "#FF6767",
+        marginTop: 8,
+        width: '100%',
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 16,
+        padding: 15,
+        textAlign: "center",
+        fontWeight: "700",
+    },
+});
+
+export default Button;
