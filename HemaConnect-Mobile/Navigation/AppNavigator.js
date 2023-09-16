32ebb9ac3c986f7aff7d431e483e3ec5debc1a80
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-16 12:18:43 +0300

    Mobile <> Adding Home Screen to App Navigator

--- HemaConnect-Mobile/Navigation/AppNavigator.js
+++ HemaConnect-Mobile/Navigation/AppNavigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from "../Screens/LoginScreen";
 import RegisterScreen from "../Screens/RegisterScreen";
+import HomeScreen from "../Screens/Home";
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ function AppNavigator() {
             <Stack.Navigator initialRouteName="LoginScreen" screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="LoginScreen" component={LoginScreen} />
                 <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+                <Stack.Screen name="HomeScreen" component={HomeScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
