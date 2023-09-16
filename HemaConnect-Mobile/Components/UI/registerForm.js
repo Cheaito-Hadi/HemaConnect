751eabf2c2186af87788db8157d4a1f1070cc7fd
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-16 13:52:52 +0300

    Mobile <> Linking Register Screen

--- HemaConnect-Mobile/Components/UI/registerForm.js
+++ HemaConnect-Mobile/Components/UI/registerForm.js
@@ -2,10 +2,19 @@ import {StyleSheet, View, Text, TextInput} from "react-native";
 import {RadioButton} from 'react-native-paper';
 import Button from "../Base/customedButton";
 import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
 
 
 const registerForm = () => {
     const navigation = useNavigation();
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [selectedBloodType, setSelectedBloodType] = useState('');
 
     const handleNavigateLogin = () => {
         navigation.navigate('LoginScreen');
@@ -24,10 +33,36 @@ const registerForm = () => {
     const renderRadioButtons = () => {
         return radioOptions.map((option, index) => (
             <View key={option.value} style={{flexDirection: 'row', alignItems: 'center', width: '25%'}}>
-                <RadioButton value={option.value}/>
+                <RadioButton
+                    value={option.value}
+                    status={selectedBloodType === option.value ? 'checked' : 'unchecked'}
+                    onPress={() => setSelectedBloodType(option.value)}
+                />
                 <Text>{option.label}</Text>
             </View>
         ));
+    };
+    const handleRegistration = async () => {
+        try {
+            const response = await axios.post('http://192.168.0.113:8000/api/register', {
+                first_name: firstName,
+                last_name: lastName,
+                email: email,
+                password: password,
+                bloodtype_id: selectedBloodType,
+                usertype_id: 1
+            },{
+                    'Content-Type': 'application/json'
+            });
+
+            const token = response.data.user.token;
+            await AsyncStorage.setItem('authToken', token);
+            console.log('AuthToken:', token);
+            navigation.navigate('HomeScreen')
+
+        } catch (error) {
+            console.error('Registration error:', error);
+        }
     };
     return (
         <View style={styles.formContainer}>
@@ -35,19 +70,27 @@ const registerForm = () => {
                 <TextInput
                     style={styles.credentials}
                     placeholder="First name"
+                    value={firstName}
+                    onChangeText={(firsName) => setFirstName(firsName)}
                 />
                 <TextInput
                     style={styles.credentials}
                     placeholder="Last name"
+                    value={lastName}
+                    onChangeText={(lastName) => setLastName(lastName)}
                 />
                 <TextInput
                     style={styles.credentials}
                     placeholder="Email"
+                    value={email}
+                    onChangeText={(email) => setEmail(email)}
                 />
                 <TextInput
                     style={styles.credentials}
                     placeholder="Password"
                     secureTextEntry={true}
+                    value={password}
+                    onChangeText={(password) => setPassword(password)}
                 />
                 <TextInput
                     style={styles.credentials}
@@ -63,6 +106,7 @@ const registerForm = () => {
                 <View style={styles.signUpBtn}>
                     <Button
                         buttonTitle="Sign Up"
+                        handlePress={handleRegistration}
                     />
                 </View>
                 <View style={styles.signUp}>
