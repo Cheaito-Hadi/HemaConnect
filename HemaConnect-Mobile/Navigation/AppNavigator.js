68c191332e6a8beecc04a89c4977ad5b02bbba5c
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-16 15:36:44 +0300

    Mobile <> Adding Appointments and Map screens to appNavigator

--- HemaConnect-Mobile/Navigation/AppNavigator.js
+++ HemaConnect-Mobile/Navigation/AppNavigator.js
@@ -4,6 +4,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from "../Screens/LoginScreen";
 import RegisterScreen from "../Screens/RegisterScreen";
 import HomeScreen from "../Screens/Home";
+import AppointmentsScreen from "../Screens/AppointmentsScreen";
+import MapsScreen from "../Screens/MapsScreen";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +16,8 @@ function AppNavigator() {
                 <Stack.Screen name="LoginScreen" component={LoginScreen} />
                 <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
                 <Stack.Screen name="HomeScreen" component={HomeScreen} />
+                <Stack.Screen name="AppointmentsScreen" component={AppointmentsScreen} />
+                <Stack.Screen name="MapsScreen" component={MapsScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
