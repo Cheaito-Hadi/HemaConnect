5253e340091fa7491a2cade2d2a0a952fdd48df6
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-17 18:46:14 +0300

    Server <> Creating hospital api

--- HemaConnect-Server/app/Http/Controllers/AdminConroller.php
+++ HemaConnect-Server/app/Http/Controllers/AdminConroller.php
@@ -7,6 +7,20 @@ use Illuminate\Http\Request;
 
 class AdminConroller extends Controller
 {
-    public function createHospital(){
+    public function createHospital(Request $request){
+        $new_hospital = new Hospital;
+        $new_hospital->name = $request->name;
+        $new_hospital->latitude = $request->latitude;
+        $new_hospital->longitude = $request->longitude;
+        $new_hospital->phone_number = $request->phone_number;
+        $file_name = time() . "post_image" . "." . $request->logo_url->extension();
+        $request->logo_url->move(storage_path('images'), $file_name);
+        $new_hospital->logo_url = storage_path("images") . "\\" . $file_name;
+        $new_hospital->save();
+
+        return response()->json([
+            "status" => "success",
+            "new_hospital" => $new_hospital
+        ]);
     }
 }
