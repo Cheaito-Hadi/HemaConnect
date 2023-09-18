439bfa1a5d7e301a207f5c77c9d8b0121678c398
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-18 20:41:50 +0300

    Mobile <> Adding Calendar

--- HemaConnect-Mobile/Screens/AppointmentsScreen.js
+++ HemaConnect-Mobile/Screens/AppointmentsScreen.js
@@ -1,19 +1,58 @@
-import {View,Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, SafeAreaView, Platform} from "react-native";
+import {Calendar, LocaleConfig} from 'react-native-calendars';
+import React, {useState} from 'react';
 
-const Appointments =()=>{
-    return(
-        <View style={styles.homeContainer}>
-            <Text>This is the Appointments Page</Text>
-        </View>
+const Appointments = () => {
+    const [selected, setSelected] = useState('');
+    const handleDayPress = (day) => {
+        setSelected(day.dateString);
+        console.log("Selected day:", day.dateString);
+    };
+    return (
+        <SafeAreaView edges={['top']} style={styles.safeAndroidView}>
+            <View style={styles.homeContainer}>
+                <View style={styles.calendarWrapper}>
+                    <Text style={styles.pickDateText}>Pick a Date</Text>
+                    <Calendar
+                        style={styles.calendarStyles}
+                        onDayPress={handleDayPress}
+                        markedDates={{
+                            [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
+                        }}
+                    />
+                </View>
+            </View>
+        </SafeAreaView>
     );
 }
 const styles = StyleSheet.create({
     homeContainer: {
+        flex: 1,
         alignItems: 'center',
+        justifyContent: 'flex-start',
+        backgroundColor:'#F7F0F3'
+    },
+    safeAndroidView: {
+        paddingTop: Platform.OS === 'android' ? 30 : 0,
+        flex: 1,
+    },
+    calendarStyles: {
+        height: 320,
+        width: '100%',
+        borderRadius: 8,
+        backgroundColor:'#fdd7d7',
+    },
+    pickDateText: {
+        fontSize: 16,
+        fontWeight: '500',
+        marginTop: 10,
+        textAlign:'justify',
+    },
+    calendarWrapper: {
         justifyContent: "center",
         width: '100%',
-        flex:1,
-    },
+        paddingHorizontal: 20,
+    }
 
 });
 
