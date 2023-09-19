fbb46315ccf8580862586635b0247ff869d683e6
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-19 10:06:36 +0300

    Mobile <> Adding dateTime picker

--- HemaConnect-Mobile/Screens/AppointmentsScreen.js
+++ HemaConnect-Mobile/Screens/AppointmentsScreen.js
@@ -1,59 +1,78 @@
-import {View, Text, StyleSheet, SafeAreaView, Platform} from "react-native";
-import {Calendar, LocaleConfig} from 'react-native-calendars';
+import {View, Text, StyleSheet, SafeAreaView, Platform, Button} from "react-native";
 import React, {useState} from 'react';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const Appointments = () => {
-    const [selected, setSelected] = useState('');
-    const handleDayPress = (day) => {
-        setSelected(day.dateString);
-        console.log("Selected day:", day.dateString);
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate;
+        setShow(false);
+        setDate(currentDate);
     };
+
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    };
+
+    const showTimepicker = () => {
+        showMode('time');
+    };
+
     return (
         <SafeAreaView edges={['top']} style={styles.safeAndroidView}>
             <View style={styles.homeContainer}>
                 <View style={styles.calendarWrapper}>
-                    <Text style={styles.pickDateText}>Pick a Date</Text>
-                    <Calendar
-                        style={styles.calendarStyles}
-                        onDayPress={handleDayPress}
-                        markedDates={{
-                            [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
-                        }}
-                    />
+                    <Text style={styles.pickDateText}>Your Next Booking</Text>
+                </View>
+                <View>
+                    <Button onPress={showDatepicker} title="Show date picker!"/>
+                    <Button onPress={showTimepicker} title="Show time picker!"/>
+                    <Text>selected: {date.toLocaleString()}</Text>
+                    {show && (
+                        <DateTimePicker
+                            testID="dateTimePicker"
+                            value={date}
+                            mode={mode}
+                            is24Hour={true}
+                            onChange={onChange}
+                        />
+                    )}
                 </View>
             </View>
         </SafeAreaView>
     );
 }
+
 const styles = StyleSheet.create({
     homeContainer: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'flex-start',
-        backgroundColor:'#F7F0F3'
+        backgroundColor: '#F7F0F3'
     },
     safeAndroidView: {
         paddingTop: Platform.OS === 'android' ? 30 : 0,
         flex: 1,
     },
-    calendarStyles: {
-        height: 320,
-        width: '100%',
-        borderRadius: 8,
-        backgroundColor:'#fdd7d7',
-    },
     pickDateText: {
         fontSize: 16,
         fontWeight: '500',
         marginTop: 10,
-        textAlign:'justify',
+        textAlign: 'justify',
     },
     calendarWrapper: {
         justifyContent: "center",
         width: '100%',
         paddingHorizontal: 20,
-    }
-
+    },
 });
 
 export default Appointments;
