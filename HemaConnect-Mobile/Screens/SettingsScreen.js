04b023ca8b1fcf100a4ae52ad0ac02ca07118e00
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-21 09:58:13 +0300

    Mobile <> Styling Settings Screen

--- HemaConnect-Mobile/Screens/SettingsScreen.js
+++ HemaConnect-Mobile/Screens/SettingsScreen.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity, Alert } from "react-native";
+import React, {useState, useEffect} from "react";
+import {View, Text, StyleSheet, Image, TouchableOpacity, Alert, SafeAreaView, Platform} from "react-native";
 import Button from "../Components/Base/customedButton";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation } from "@react-navigation/native";
+import {useNavigation} from "@react-navigation/native";
 import * as ImagePicker from 'expo-image-picker';
 import axios from 'axios';
+import {Ionicons} from "@expo/vector-icons";
 
 const Settings = () => {
     const navigation = useNavigation();
@@ -43,7 +44,7 @@ const Settings = () => {
 
         const result = await ImagePicker.launchImageLibraryAsync();
 
-        if (!result.cancelled) {
+        if (!result.canceled) {
             setImage(result.assets[0].uri);
             await uploadImage(result.assets[0].uri);
         }
@@ -72,66 +73,93 @@ const Settings = () => {
     };
 
     return (
-        <View style={styles.homeContainer}>
-            <View style={styles.profileContainer}>
-                <TouchableOpacity onPress={pickImage}>
-                    <Image
-                        source={{ uri: `http://192.168.0.113:8000/storage/${userData?.image_url}` }}
-                        style={styles.profileImage}
+        <SafeAreaView edges={['top']} style={styles.safeAndroidView}>
+            <View style={styles.titleContainer}>
+                <Text style={styles.textTitle}>My Profile</Text>
+            </View>
+            <View style={styles.homeContainer}>
+                <View style={styles.profileContainer}>
+                    <TouchableOpacity onPress={pickImage}>
+                        <Image
+                            source={{uri: `http://192.168.0.113:8000/storage/${userData?.image_url}`}}
+                            style={styles.profileImage}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.uploadButton}
+                        onPress={pickImage}
+                    >
+                        <Text style={styles.uploadButtonText}>Change Profile Image</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.logOut}>
+                    <Button
+                        buttonTitle="Logout"
+                        handlePress={handleLogout}
                     />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={styles.uploadButton}
-                    onPress={pickImage}
-                >
-                    <Text style={styles.uploadButtonText}>Upload Image</Text>
-                </TouchableOpacity>
+                </View>
             </View>
-            <View style={styles.logOut}>
-                <Button
-                    buttonTitle="Logout"
-                    handlePress={handleLogout}
-                />
-            </View>
-        </View>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
     homeContainer: {
         alignItems: 'center',
-        justifyContent: "center",
+        justifyContent: "flex-start",
+        marginTop:'10%',
         width: '100%',
         flex: 1,
     },
     profileContainer: {
-        alignItems: 'center',
-        borderWidth:1,
-        borderColor:'#000',
-        borderRadius:10,
-        width: 150,
-        height: 150,
+        borderRadius: 100,
+        width: 200,
+        height: 200,
     },
     profileImage: {
-        width: 150,
-        height: 150,
-        borderRadius: 75,
+        width: 200,
+        height: 200,
+        borderRadius: 100,
         marginBottom: 20,
     },
     uploadButton: {
-        backgroundColor: 'blue',
+        backgroundColor: '#ff6767',
         paddingVertical: 10,
         paddingHorizontal: 20,
         borderRadius: 5,
+        justifyContent:"center",
+        alignItems:"center",
     },
     uploadButtonText: {
-        color: 'white',
+        color: '#FFF',
+        fontSize: 16,
+        fontWeight: '500',
     },
     logOut: {
         position: 'absolute',
         bottom: 20,
         right: 20,
     },
+    safeAndroidView: {
+        paddingTop: Platform.OS === 'android' ? 30 : 0,
+        flex: 1,
+    },
+    titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: "center",
+        backgroundColor: '#FF6767',
+        height: 80,
+        borderBottomRightRadius: 15,
+        borderBottomLeftRadius: 15,
+    },
+    textTitle: {
+        alignItems: "center",
+        justifyContent: "center",
+        color: '#FFF',
+        fontWeight: '500',
+        fontSize: 20,
+    }
 });
 
 export default Settings;
