e99971b225090b39a24514db5b1fd586151a2a48
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-24 20:14:01 +0300

    Client <> Creating Hospital with Modal Linked

--- hemaconnect-client/src/components/ui/adminTable/index.jsx
+++ hemaconnect-client/src/components/ui/adminTable/index.jsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import "./styles.css";
-import { UserPlusIcon } from "@heroicons/react/24/solid";
 import {
     Card,
     CardHeader,
@@ -14,14 +13,14 @@ import HospitalModal from "../hospitalModal";
 
 const TABLE_HEAD = ["Hospital", "Phone Number", "Address", "Add Employee"];
 
-const AdminTable = ()=>{
+const AdminTable = () => {
     const [hospitalData, setHospitalData] = useState([]);
     const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen((cur) => !cur);
+   const handleOpen = () => setOpen((cur) => !cur);
 
-    async function fetchHospitalData () {
+    async function fetchHospitalData() {
         try {
-            const response =await axios.get('http://127.0.0.1:8000/api/getAllhospitals', {
+            const response = await axios.get('http://127.0.0.1:8000/api/getAllhospitals', {
                 headers: {
                     Authorization: `Bearer ${localStorage.getItem("token")}`
                 }
@@ -32,108 +31,109 @@ const AdminTable = ()=>{
         }
     }
 
-    useEffect(() => {
-        fetchHospitalData();
-    }, []);
+useEffect(() => {
+    fetchHospitalData();
+}, []);
 
-    return(
-        <Card className="h-full w-[95%] mt-5 overflow-auto">
-            <CardHeader floated={false} shadow={false} className="rounded-none">
-                <div className="mb-8 flex items-center justify-between gap-8">
+return (
+    <Card className="h-full w-[95%] mt-5 overflow-auto">
+        <CardHeader floated={false} shadow={false} className="rounded-none">
+            <div className="mb-8 flex items-center justify-between gap-8">
 
-                    <div>
-                        <Typography variant="h5" color="blue-gray">
-                            Hospital List
-                        </Typography>
-                        <Typography color="gray" className="mt-1 font-normal">
-                            See information about all hospitals
-                        </Typography>
-                    </div>
-                    <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
-                        <Button className="flex items-center gap-3" size="sm">
-                            <HospitalModal />
-                        </Button>
-                    </div>
+                <div>
+                    <Typography variant="h5" color="blue-gray">
+                        Hospital List
+                    </Typography>
+                    <Typography color="gray" className="mt-1 font-normal">
+                        See information about all hospitals
+                    </Typography>
                 </div>
-            </CardHeader>
-            <CardBody className="overflow-scroll px-0">
-                <table className="mt-4 w-full min-w-max table-auto text-left">
-                    <thead>
-                    <tr>
-                        {TABLE_HEAD.map((head) => (
-                            <th
-                                key={head}
-                                className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
+                <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+                    <Button className="flex items-center gap-3" size="sm">
+                        <HospitalModal/>
+                    </Button>
+                </div>
+            </div>
+        </CardHeader>
+        <CardBody className="overflow-scroll px-0">
+            <table className="mt-4 w-full min-w-max table-auto text-left">
+                <thead>
+                <tr>
+                    {TABLE_HEAD.map((head) => (
+                        <th
+                            key={head}
+                            className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
+                        >
+                            <Typography
+                                variant="small"
+                                color="blue-gray"
+                                className="font-normal leading-none opacity-70"
                             >
-                                <Typography
-                                    variant="small"
-                                    color="blue-gray"
-                                    className="font-normal leading-none opacity-70"
-                                >
-                                    {head}
-                                </Typography>
-                            </th>
-                        ))}
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {hospitalData.map(
-                        ({ name, logo_url, phone_number,latitude, longitude }, index) => {
-                            const isLast = index === hospitalData.length - 1;
-                            const classes = isLast
-                                ? "p-4"
-                                : "p-4 border-b border-blue-gray-50";
+                                {head}
+                            </Typography>
+                        </th>
+                    ))}
+                </tr>
+                </thead>
+                <tbody>
+                {hospitalData.map(
+                    ({name, logo_url, phone_number, latitude, longitude}, index) => {
+                        const isLast = index === hospitalData.length - 1;
+                        const classes = isLast
+                            ? "p-4"
+                            : "p-4 border-b border-blue-gray-50";
 
-                            return (
-                                <tr key={index}>
-                                    <td className={classes}>
-                                        <div className="flex items-center gap-3">
-                                            <Avatar src={`http://127.0.0.1:8000/storage/${logo_url}`} alt={logo_url} size="sm" />
-                                            <div className="flex flex-col">
-                                                <Typography
-                                                    variant="small"
-                                                    color="blue-gray"
-                                                    className="font-normal"
-                                                >
-                                                    {name}
-                                                </Typography>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td className={classes}>
+                        return (
+                            <tr key={index}>
+                                <td className={classes}>
+                                    <div className="flex items-center gap-3">
+                                        <Avatar src={`http://127.0.0.1:8000/storage/${logo_url}`} alt={logo_url}
+                                                size="sm"/>
                                         <div className="flex flex-col">
                                             <Typography
                                                 variant="small"
                                                 color="blue-gray"
                                                 className="font-normal"
                                             >
-                                                {phone_number}
+                                                {name}
                                             </Typography>
                                         </div>
-                                    </td>
-                                    <td className={classes}>
+                                    </div>
+                                </td>
+                                <td className={classes}>
+                                    <div className="flex flex-col">
                                         <Typography
                                             variant="small"
                                             color="blue-gray"
                                             className="font-normal"
                                         >
-                                            {latitude}-{longitude}
+                                            {phone_number}
                                         </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Button>
-                                            Add Employee
-                                        </Button>
-                                    </td>
-                                </tr>
-                            );
-                        },
-                    )}
-                    </tbody>
-                </table>
-            </CardBody>
-        </Card>
-    );
+                                    </div>
+                                </td>
+                                <td className={classes}>
+                                    <Typography
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="font-normal"
+                                    >
+                                        {latitude}-{longitude}
+                                    </Typography>
+                                </td>
+                                <td className={classes}>
+                                    <Button>
+                                        Add Employee
+                                    </Button>
+                                </td>
+                            </tr>
+                        );
+                    },
+                )}
+                </tbody>
+            </table>
+        </CardBody>
+    </Card>
+);
 }
 
 export default AdminTable;
--- hemaconnect-client/src/components/ui/hospitalModal/index.jsx
+++ hemaconnect-client/src/components/ui/hospitalModal/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
     Button,
     Dialog,
@@ -14,18 +14,57 @@ import { TileLayer } from 'react-leaflet/TileLayer'
 import { Marker, Popup } from 'react-leaflet'
 import {Icon} from 'leaflet'
 import "leaflet/dist/leaflet.css";
+import axios from "axios";
 
-const HospitalModal = ({handleLatLng})=>{
+const HospitalModal = ()=>{
     const [open, setOpen] = React.useState(false);
+    const [newHospital, setNewHospital] = useState({
+        name: "",
+        phone_number:"",
+        longitude:33.8938,
+        latitude:35.5018,
+    });
+    const [logo, setLogo] = useState(null)
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        setLogo( file );
+    };
 
     const LeafletMarker = new Icon({
         iconSize:     [25, 25],
         iconUrl: 'https://img.icons8.com/?size=512&id=PZTTDl8ML4vy&format=png'
     });
 
+    const handleDataChange = (e) => {
+        setNewHospital({ ...newHospital, [e.target.name]: e.target.value });
+    };
+
+    const createHospital = async () => {
+        // console.log(newHospital)
+        const formData = new FormData()
+        formData.append('name', newHospital.name)
+        formData.append('longitude', newHospital.longitude)
+        formData.append('latitude', newHospital.latitude)
+        formData.append('phone_number', newHospital.phone_number)
+        formData.append('logo_url', logo)
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/create_hospital', formData, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`
+                },
+            });
+        } catch (error) {
+            console.error('Error creating blood request:', error);
+        }
+    };
+
     const handleMarkerEvent = (e) => {
-        handleLatLng(e.target._latlng.lat, e.target._latlng.lng)
-    }
+        setNewHospital({
+            ...newHospital,
+            latitude: e.target._latlng.lat,
+            longitude: e.target._latlng.lng
+        });
+    };
     const handleOpen = () => setOpen((cur) => !cur);
 
     return(
@@ -48,25 +87,22 @@ const HospitalModal = ({handleLatLng})=>{
                         </Typography>
                     </CardHeader>
                     <CardBody className="flex flex-col gap-4">
-                        <Input label="Name" size="lg" />
-                        <Input label="Phone Number" size="lg" />
-                        <Input label="Logo" size="lg" type="file"/>
+                        <Input label="Name" size="lg" name={"name"} value={newHospital.name} onChange={handleDataChange}/>
+                        <Input label="Phone Number" size="lg" name={"phone_number"} value={newHospital.phone_number} onChange={handleDataChange}/>
+                        <Input label="Logo" size="lg" type="file" name={"logo_url"} onChange={handleFileChange}/>
                     <div className="w-[100%] h-60">
                         <MapContainer center={[33.8938, 35.5018]} zoom={10} scrollWheelZoom={true} className="w-full h-full">
                             <TileLayer
                                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                             />
-                            <Marker position={[33.8938, 35.5018]} draggable={true} icon={LeafletMarker} eventHandlers={{dragend: handleMarkerEvent}}>
-                                <Popup>
-                                    A pretty CSS3 popup. <br /> Easily customizable.
-                                </Popup>
+                            <Marker position={[newHospital.latitude, newHospital.longitude]} draggable={true} icon={LeafletMarker} eventHandlers={{dragend: handleMarkerEvent}}>
                             </Marker>
                         </MapContainer>
                     </div>
                     </CardBody>
                     <CardFooter className="pt-0">
-                        <Button variant="gradient" color="red" onClick={handleOpen} fullWidth>
+                        <Button variant="gradient" color="red" onClick={createHospital} fullWidth>
                             Create
                         </Button>
                     </CardFooter>
