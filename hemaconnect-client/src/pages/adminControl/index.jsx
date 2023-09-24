875db8c4a859c7e927f605e3e4164b94b12b5436
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-24 15:52:29 +0300

    Client <> Creating adminControl Page

---
+++ hemaconnect-client/src/pages/adminControl/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles.css";
+
+const adminControl = ()=>{
+    return(
+        <div>
+            Admin Page
+        </div>
+    );
+}
+
+export default adminControl;
