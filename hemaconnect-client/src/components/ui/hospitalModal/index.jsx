b271fc0f6d9928ff2ef88840e5c8eb800a6b3bca
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-24 18:40:11 +0300

    Client <> Adding Leaflet Map to Modal

--- hemaconnect-client/src/components/ui/hospitalModal/index.jsx
+++ hemaconnect-client/src/components/ui/hospitalModal/index.jsx
@@ -10,20 +10,26 @@ import {
     Input,
     Checkbox,
 } from "@material-tailwind/react";
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { Marker, Popup } from 'react-leaflet'
+import "leaflet/dist/leaflet.css";
+import { useState } from 'react';
 
 const HospitalModal = ()=>{
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen((cur) => !cur);
+
     return(
         <>
             <Button onClick={handleOpen}>Sign In</Button>
             <Dialog
-                size="xs"
+                size="md"
                 open={open}
                 handler={handleOpen}
                 className="bg-transparent shadow-none"
             >
-                <Card className="mx-auto w-full max-w-[24rem]">
+                <Card className="mx-auto w-full max-w-full">
                     <CardHeader
                         variant="gradient"
                         color="red"
@@ -37,7 +43,19 @@ const HospitalModal = ()=>{
                         <Input label="Name" size="lg" />
                         <Input label="Phone Number" size="lg" />
                         <Input label="Logo" size="lg" type="file"/>
-
+                    <div className="w-[100%] h-60">
+                        <MapContainer center={[34, 36]} zoom={10} scrollWheelZoom={true} className="w-full h-full">
+                            <TileLayer
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={[34, 36]} draggable={true}>
+                                <Popup>
+                                    A pretty CSS3 popup. <br /> Easily customizable.
+                                </Popup>
+                            </Marker>
+                        </MapContainer>
+                    </div>
                     </CardBody>
                     <CardFooter className="pt-0">
                         <Button variant="gradient" color="red" onClick={handleOpen} fullWidth>
