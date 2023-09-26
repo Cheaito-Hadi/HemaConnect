a9ed5e9fb01558e17b15ce67feb8a0feb0e480f4
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-26 13:22:36 +0300

    Client <> Close modal on create

--- hemaconnect-client/src/components/ui/hospitalModal/index.jsx
+++ hemaconnect-client/src/components/ui/hospitalModal/index.jsx
@@ -25,6 +25,7 @@ const HospitalModal = ()=>{
         latitude:35.5018,
     });
     const [logo, setLogo] = useState(null)
+    const [errors, setErrors] = useState({});
     const handleFileChange = (e) => {
         const file = e.target.files[0];
         setLogo( file );
@@ -37,9 +38,24 @@ const HospitalModal = ()=>{
 
     const handleDataChange = (e) => {
         setNewHospital({ ...newHospital, [e.target.name]: e.target.value });
+        setErrors({ ...errors, [e.target.name]: "" });
     };
 
     const createHospital = async () => {
+        const validationErrors = {};
+        if (!newHospital.name.trim()) {
+            validationErrors.name = "Name is required";
+        }
+        if (!newHospital.phone_number.trim()) {
+            validationErrors.phone_number = "Phone Number is required";
+        }
+        if (!logo) {
+            validationErrors.logo_url = "Logo is required";
+        }
+        if (Object.keys(validationErrors).length > 0) {
+            setErrors(validationErrors);
+            return;
+        }
 
         const addressData = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${newHospital.latitude},${newHospital.longitude}&key=AIzaSyDHUtORXXAhUn_c-UbE_6pWSPXzeKwrZpc`)
 
@@ -57,6 +73,9 @@ const HospitalModal = ()=>{
                     Authorization: `Bearer ${localStorage.getItem("token")}`
                 },
             });
+            if (response.status === 200) {
+                setOpen((cur) => !cur)
+            }
         } catch (error) {
             console.error('Error creating blood request:', error);
         }
@@ -80,30 +99,33 @@ const HospitalModal = ()=>{
                 handler={handleOpen}
                 className="bg-transparent shadow-none"
             >
-                <Card className="mx-auto w-full max-w-full">
+                <Card className="mx-auto w-full max-w-full mb-0.5">
                     <CardHeader
                         variant="gradient"
                         color="red"
-                        className="mb-3 grid h-20 place-items-center"
+                        className="mb-0.5 mt-0.3 grid h-20 place-items-center"
                     >
                         <Typography variant="h5" color="white">
                             Add Hospital
                         </Typography>
                     </CardHeader>
                     <CardBody className="flex flex-col gap-4">
-                        <Input label="Name" size="lg" name={"name"} value={newHospital.name} onChange={handleDataChange}/>
-                        <Input label="Phone Number" size="lg" name={"phone_number"} value={newHospital.phone_number} onChange={handleDataChange}/>
-                        <Input label="Logo" size="lg" type="file" name={"logo_url"} onChange={handleFileChange}/>
-                    <div className="w-[100%] h-60">
-                        <MapContainer center={[33.8938, 35.5018]} zoom={10} scrollWheelZoom={true} className="w-full h-full">
-                            <TileLayer
-                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                            />
-                            <Marker position={[newHospital.latitude, newHospital.longitude]} draggable={true} icon={LeafletMarker} eventHandlers={{dragend: handleMarkerEvent}}>
-                            </Marker>
-                        </MapContainer>
-                    </div>
+                        <Input label="Name" size="lg" name={"name"} value={newHospital.name} onChange={handleDataChange} />
+                        {errors.name && <span className="text-red-500">{errors.name}</span>}
+                        <Input label="Phone Number" size="lg" name={"phone_number"} value={newHospital.phone_number} onChange={handleDataChange} />
+                        {errors.phone_number && <span className="text-red-500">{errors.phone_number}</span>}
+                        <Input label="Logo" size="lg" type="file" name={"logo_url"} onChange={handleFileChange} />
+                        {errors.logo_url && <span className="text-red-500">{errors.logo_url}</span>}
+                        <div className="w-[100%] h-60">
+                            <MapContainer center={[newHospital.latitude, newHospital.longitude]} zoom={10} scrollWheelZoom={true} className="w-full h-full">
+                                <TileLayer
+                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                />
+                                <Marker position={[newHospital.latitude, newHospital.longitude]} draggable={true} icon={LeafletMarker} eventHandlers={{ dragend: handleMarkerEvent }}>
+                                </Marker>
+                            </MapContainer>
+                        </div>
                     </CardBody>
                     <CardFooter className="pt-0">
                         <Button variant="gradient" color="red" onClick={createHospital} fullWidth>
