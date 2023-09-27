0eec1bb32117d52785458131324b18385338fdf2
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-27 14:47:15 +0300

    Mobile <> Adding Lottie Animations to the OnBoarding

--- HemaConnect-Mobile/onBoarding.js
+++ HemaConnect-Mobile/onBoarding.js
@@ -1,67 +1,121 @@
 import Onboarding from "react-native-onboarding-swiper";
-import {Image, TouchableOpacity, Text} from "react-native";
-import React from 'react';
+import {Image, TouchableOpacity, Text, View, StyleSheet} from "react-native";
+import React, {useRef} from 'react';
+import Lottie from 'lottie-react-native';
 
 
+const Dots = ({selected}) => {
+    const animation = useRef(null);
+    let backgroundColor;
+
+    backgroundColor = selected ? 'rgba(0, 0, 0, 0.8)' : 'rgba(0, 0, 0, 0.3)';
+
+    return (
+        <View
+            style={{
+                width: 6,
+                height: 6,
+                marginHorizontal: 3,
+                backgroundColor
+            }}
+        />
+    );
+}
 const Skip = ({...props}) => (
     <TouchableOpacity
-        style={{marginHorizontal:10}}
+        style={{marginHorizontal: 10}}
         {...props}
     >
-        <Text style={{fontSize:16}}>Skip</Text>
+        <Text style={{fontSize: 16}}>Skip</Text>
     </TouchableOpacity>
 );
 
 const Next = ({...props}) => (
     <TouchableOpacity
-        style={{marginHorizontal:10}}
+        style={{marginHorizontal: 10}}
         {...props}
     >
-        <Text style={{fontSize:16}}>Next</Text>
+        <Text style={{fontSize: 16}}>Next</Text>
     </TouchableOpacity>
 );
 
 const Done = ({...props}) => (
     <TouchableOpacity
-        style={{marginHorizontal:10}}
+        style={{marginHorizontal: 10}}
         {...props}
     >
-        <Text style={{fontSize:16}}>Done</Text>
+        <Text style={{fontSize: 16}}>Done</Text>
     </TouchableOpacity>
 );
 
 const OnBoarding = ({navigation}) => {
     return (
-        <Onboarding
-            SkipButtonComponent={Skip}
-            NextButtonComponent={Next}
-            DoneButtonComponent={Done}
-            onSkip={() => navigation.replace("OnBoarding")}
-            onDone={() => navigation.navigate("OnBoarding")}
-            pages={[
-                {
-                    backgroundColor: '#a6e4d0',
-                    image: <Image source={require('./assets/Pictures/default.jpg')} />,
-                    title: 'Connect to the World',
-                    subtitle: 'A New Way To Connect With The World',
-                },
-                {
-                    backgroundColor: '#fdeb93',
-                    image: <Image source={require('./assets/Pictures/default.jpg')} />,
-                    title: 'Share Your Favorites',
-                    subtitle: 'Share Your Thoughts With Similar Kind of People',
-                },
-                {
-                    backgroundColor: '#e9bcbe',
-                    image: <Image source={require('./assets/Pictures/default.jpg')} />,
-                    title: 'Become The Star',
-                    subtitle: "Let The Spot Light Capture You",
-                },
-            ]}
-        />
-
+        <View style={styles.onBoardingContainer}>
+            <Onboarding
+                SkipButtonComponent={Skip}
+                NextButtonComponent={Next}
+                DoneButtonComponent={Done}
+                DotComponent={Dots}
+                onSkip={() => navigation.replace("LoginScreen")}
+                onDone={() => navigation.navigate("LoginScreen")}
+                containerStyles={{paddingHorizontal: 15}}
+                pages={[
+                    {
+                        backgroundColor: '#a6e4d0',
+                        image: (
+                            <View style={styles.animationContainer}>
+                                <Lottie
+                                    autoPlay loop
+                                    source={require('./assets/Animations/bloodBag.json')}
+                                />
+                            </View>
+                        ),
+                        title: 'Connect to the World',
+                        subtitle: 'A New Way To Connect With The World',
+                    },
+                    {
+                        backgroundColor: '#a6e4d0',
+                        image: (
+                            <View style={styles.animationContainer}>
+                                <Lottie
+                                    autoPlay loop
+                                    source={require('./assets/Animations/heart.json')}
+                                />
+                            </View>
+                        ),
+                        title: 'Connect to the World',
+                        subtitle: 'A New Way To Connect With The World',
+                    },
+                    {
+                        backgroundColor: '#a6e4d0',
+                        image: (
+                            <View style={styles.animationContainer}>
+                                <Lottie
+                                    autoPlay loop
+                                    source={require('./assets/Animations/bloodSample.json')}
+                                />
+                            </View>
+                        ),
+                        title: 'Connect to the World',
+                        subtitle: 'A New Way To Connect With The World',
+                    },
+                ]}
+            />
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+
+    onBoardingContainer:{
+        flex: 1,
+    },
+    animationContainer:{
+        width:300,
+        height:300,
+        backgroundColor:'white',
+    }
+})
 
 export default OnBoarding;
 
