a7561139ec74900e495f20da0b94e91218e49635
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-09-27 16:26:55 +0300

    Mobile <> Adding Loader

--- HemaConnect-Mobile/Navigation/AppNavigator.js
+++ HemaConnect-Mobile/Navigation/AppNavigator.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from "../Screens/LoginScreen";
@@ -8,45 +8,34 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import OnBoarding from "../onBoarding";
 
 const Stack = createStackNavigator();
-const getIsSignedIn = async () => {
-    try {
-        const token = await AsyncStorage.getItem('authToken');
-        return !!token;
-    } catch (error) {
-        console.error(error);
-        return false;
-    }
-}
 
 function AppNavigator() {
     const navigationRef = useRef();
     const [isSignedIn, setIsSignedIn] = useState(false);
-    useEffect(() => {
-        async function checkAuthentication() {
-            const signedIn = await getIsSignedIn();
-            setIsSignedIn(signedIn);
-        }
-        checkAuthentication();
-    }, []);
-    useEffect(() => {
-        const checkAuthentication = async () => {
-            try {
-                const userToken = await AsyncStorage.getItem('authToken');
-                if (userToken) {
-                    navigationRef.current?.navigate('BottomNavigator');
-                } else {
-                    navigationRef.current?.navigate('OnBoarding');
-                }
-            } catch (error) {
+
+    const checkAuthentication = async () => {
+        try {
+            const userToken = await AsyncStorage.getItem('authToken');
+            if (userToken) {
+                setIsSignedIn(isSignedIn);
+                navigationRef.current?.navigate('BottomNavigator');
+            } else {
+                navigationRef.current?.navigate('OnBoarding');
             }
-        };
+        } catch (error) {
+        }
+    };
+    useEffect(() => {
         checkAuthentication();
-    }, []);
+    }, [isSignedIn]);
+
     return (
         <NavigationContainer ref={navigationRef}>
             <Stack.Navigator
                 screenOptions={{headerShown: false}}>
-                <Stack.Screen name="OnBoarding" component={OnBoarding}/>
+                {
+                    !isSignedIn && <Stack.Screen name="OnBoarding" component={OnBoarding}/>
+                }
                 <Stack.Screen name="LoginScreen" component={LoginScreen}/>
                 <Stack.Screen name="RegisterScreen" component={RegisterScreen}/>
                 <Stack.Screen name="BottomNavigator" component={BottomNavigator}/>
--- HemaConnect-Mobile/onBoarding.js
+++ HemaConnect-Mobile/onBoarding.js
@@ -1,7 +1,9 @@
 import Onboarding from "react-native-onboarding-swiper";
-import {Image, TouchableOpacity, Text, View, StyleSheet} from "react-native";
-import React, {useRef} from 'react';
+import {TouchableOpacity, Text, View, StyleSheet} from "react-native";
+import React, {useRef, useEffect, useState} from 'react';
 import Lottie from 'lottie-react-native';
+import Hema from "./assets/SVGs/Hema.svg";
+import Connect from "./assets/SVGs/Connect.svg";
 
 
 const Dots = ({selected}) => {
@@ -48,97 +50,126 @@ const Done = ({...props}) => (
     </TouchableOpacity>
 );
 
+
 const OnBoarding = ({navigation}) => {
-    return (
-        <View style={styles.onBoardingContainer}>
-            <Onboarding
-                SkipButtonComponent={Skip}
-                NextButtonComponent={Next}
-                DoneButtonComponent={Done}
-                DotComponent={Dots}
-                onSkip={() => navigation.replace("LoginScreen")}
-                onDone={() => navigation.navigate("LoginScreen")}
-                containerStyles={{paddingHorizontal: 15}}
-                pages={[
-                    {
-                        backgroundColor: '#FDD7D7FF',
-                        image: (
-                            <View style={styles.animationContainer}>
-                                <Lottie
-                                    autoPlay loop
-                                    source={require('./assets/Animations/bloodBag.json')}
-                                />
-                            </View>
-                        ),
-                        title: (
-                            <View>
-                                <Text style={styles.onBoardingTitleText}>The Power of Giving: Donate Blood, Save Lives</Text>
-                                <Text style={styles.OnBoardingSubtitleText}>Uncover the Vital Role You Play in Ensuring a
-                                    Healthier, Happier Tomorrow</Text>
-                            </View>
-                        )
-                    },
-                    {
-                        backgroundColor: '#D7FDFD',
-                        image: (
-                            <View style={styles.animationContainer}>
-                                <Lottie
-                                    autoPlay loop
-                                    source={require('./assets/Animations/heart.json')}
-                                />
-                            </View>
-                        ),
-                        title: (
-                            <View>
-                                <Text style={styles.onBoardingTitleText}>Saving Lives, One Drop at a Time</Text>
-                                <Text style={styles.OnBoardingSubtitleText}>Discover the Importance of Blood Donation and Join the Lifesaving Community</Text>
-                            </View>
-                        ),
-                    },
-                    {
-                        backgroundColor: '#fdffe2',
-                        image: (
-                            <View style={styles.animationContainer}>
-                                <Lottie
-                                    autoPlay loop
-                                    source={require('./assets/Animations/bloodSample.json')}
-                                />
-                            </View>
-                        ),
-                        title: (
-                            <View>
-                                <Text style={styles.onBoardingTitleText}>Be a Hero Today: Donate Blood</Text>
-                                <Text style={styles.OnBoardingSubtitleText}>Your Gift of Blood Can Make a World of Difference</Text>
-                            </View>
-                        ),
-                    },
-                ]}
-            />
-        </View>
-    )
-}
+    const [loading, setLoading] = useState(true)
 
-const styles = StyleSheet.create({
+    const timeOut = () => { setTimeout(()=>{
+        setLoading(false)
+    }, 2000)}
 
-    onBoardingContainer:{
-        flex: 1,
-    },
-    animationContainer:{
-        width:300,
-        height:300,
-    },
-    onBoardingTitleText:{
-        fontSize:20,
-        fontWeight:"600",
-        textAlign:"center"
-    },
-    OnBoardingSubtitleText:{
-        fontSize:16,
-        fontWeight:"400",
-        textAlign:"center",
-        marginTop:10
+    useEffect(() => {
+        timeOut()
+    }, []);
+        return (
+            <View style={styles.onBoardingContainer}>
+                {
+                    loading ? <View style={styles.onBoardingContainer}>
+                            <View style={styles.logoWrapper}>
+                                <Hema width={100} height={50}/>
+                                <Connect width={150} height={50}/>
+                            </View>
+                    </View> :
+
+                <Onboarding
+                    SkipButtonComponent={Skip}
+                    NextButtonComponent={Next}
+                    DoneButtonComponent={Done}
+                    DotComponent={Dots}
+                    onSkip={() => navigation.replace("LoginScreen")}
+                    onDone={() => navigation.navigate("LoginScreen")}
+                    containerStyles={{paddingHorizontal: 15}}
+                    pages={[
+                        {
+                            backgroundColor: '#FDD7D7FF',
+                            image: (
+                                <View style={styles.animationContainer}>
+                                    <Lottie
+                                        autoPlay loop
+                                        source={require('./assets/Animations/bloodBag.json')}
+                                    />
+                                </View>
+                            ),
+                            title: (
+                                <View>
+                                    <Text style={styles.onBoardingTitleText}>The Power of Giving: Donate Blood, Save
+                                        Lives</Text>
+                                    <Text style={styles.OnBoardingSubtitleText}>Uncover the Vital Role You Play in
+                                        Ensuring a
+                                        Healthier, Happier Tomorrow</Text>
+                                </View>
+                            )
+                        },
+                        {
+                            backgroundColor: '#D7FDFD',
+                            image: (
+                                <View style={styles.animationContainer}>
+                                    <Lottie
+                                        autoPlay loop
+                                        source={require('./assets/Animations/heart.json')}
+                                    />
+                                </View>
+                            ),
+                            title: (
+                                <View>
+                                    <Text style={styles.onBoardingTitleText}>Saving Lives, One Drop at a Time</Text>
+                                    <Text style={styles.OnBoardingSubtitleText}>Discover the Importance of Blood
+                                        Donation and Join the Lifesaving Community</Text>
+                                </View>
+                            ),
+                        },
+                        {
+                            backgroundColor: '#fdffe2',
+                            image: (
+                                <View style={styles.animationContainer}>
+                                    <Lottie
+                                        autoPlay loop
+                                        source={require('./assets/Animations/bloodSample.json')}
+                                    />
+                                </View>
+                            ),
+                            title: (
+                                <View>
+                                    <Text style={styles.onBoardingTitleText}>Be a Hero Today: Donate Blood</Text>
+                                    <Text style={styles.OnBoardingSubtitleText}>Your Gift of Blood Can Make a World of
+                                        Difference</Text>
+                                </View>
+                            ),
+                        },
+                    ]}
+                />}
+            </View>
+        )
     }
-})
+
+    const styles = StyleSheet.create({
+
+        onBoardingContainer: {
+            flex: 1,
+        },
+        animationContainer: {
+            width: 300,
+            height: 300,
+        },
+        onBoardingTitleText: {
+            fontSize: 20,
+            fontWeight: "600",
+            textAlign: "center"
+        },
+        OnBoardingSubtitleText: {
+            fontSize: 16,
+            fontWeight: "400",
+            textAlign: "center",
+            marginTop: 10
+        },
+        logoWrapper:{
+            flexDirection:"row",
+            width:'100%',
+            alignItems:"center",
+            justifyContent:"center",
+            height:"100%"
+        },
+    })
 
 export default OnBoarding;
 
